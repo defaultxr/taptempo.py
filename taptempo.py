be6944c2e3aa342d087119f0f181c60917f260c8
modula t. worm <defaultxr at g mail>
2016-04-06 22:35:47 -0500

    fix

--- taptempo.py
+++ taptempo.py
@@ -20,18 +20,18 @@ if __name__ == '__main__':
  while True:
   char = getchar()
   if char in ('q', 'Q', '\x1b', '\x03'): # q, Q, ESC, Control+C
+   print()
    quit()
   else:
    array.append(time())
-   if len(array) > 4:
+   if len(array) > 16:
     del array[0]
    if len(array) < 2:
-    print('Keep tapping on the beats. Press q to quit.', end='\r')
+    print('Keep tapping on beat. Press q to quit.    ', end='\r')
    else:
     averagetime = 0
     for i in range(len(array)-1):
      averagetime += (array[i+1]-array[i])
-    averagetime = averagetime / ( (len(array)-1) * 4)
-    averagetime *= 1000
-    bpm = (60000.0/averagetime)/4.0
-    print("\rDetected BPM: %.3f (Avg time between each: %.3fms)" % (bpm, averagetime), end='')
+    averagetime = averagetime / (len(array)-1)
+    bpm = (1.0/(averagetime/60.0))
+    print("\rDetected BPM: %0.3f (Avg time between each: %0.3fs)" % (bpm, averagetime), end='')

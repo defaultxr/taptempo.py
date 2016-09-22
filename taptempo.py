ef9cf45b6723233089c2568b10abaf0a9b46da78
Alex Gottschalk <alex.gottschalk@invitae.com>
2016-09-21 17:55:36 -0700

    major cleanup

--- taptempo.py
+++ taptempo.py
@@ -1,37 +1,52 @@
 #!/usr/bin/env python
-import tty, termios
+
+from __future__ import print_function
+
+import tty
+import termios
 from time import time
 from sys import stdin
 
+
 def getchar():
- fd = stdin.fileno()
- old_settings = termios.tcgetattr(fd)
- try:
-  tty.setraw(stdin.fileno())
-  ch = stdin.read(1)
- finally:
-  termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-  return ch
+    fd = stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(stdin.fileno())
+        ch = stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
 
-if __name__ == '__main__':
- print('Tap a key on each beat. Press q to quit.', end='\r')
- array = []
 
- while True:
-  char = getchar()
-  if char in ('q', 'Q', '\x1b', '\x03'): # q, Q, ESC, Control+C
-   print()
-   quit()
-  else:
-   array.append(time())
-   if len(array) > 16:
-    del array[0]
-   if len(array) < 2:
-    print('Keep tapping on beat. Press q to quit.    ', end='\r')
-   else:
-    averagetime = 0
-    for i in range(len(array)-1):
-     averagetime += (array[i+1]-array[i])
-    averagetime = averagetime / (len(array)-1)
-    bpm = (1.0/(averagetime/60.0))
-    print("\rDetected BPM: %0.3f (Avg time between each: %0.3fs)" % (bpm, averagetime), end='')
+def addtime(times):
+    if type(times) != list:
+        raise(TypeError)
+    t = time()
+    if len(times) == 0:
+        tdiff = 0
+    else:
+        tdiff = t - times[-1][0]
+    return (t, tdiff)
+
+
+print('Tap a key on each beat. Press q to quit.')
+
+TIMES = []
+while True:
+    char = getchar()
+    if char in ('q', 'Q', '\x1b', '\x03'):  # q, Q, ESC, Control+C
+        print()
+        quit()
+
+    TIMES.append(addtime(TIMES))
+    if len(TIMES) > 16:
+        del TIMES[0]
+    if len(TIMES) > 1:
+        if TIMES[0][1] == 0:
+            del TIMES[0]
+        averagetime = sum([row[1] for row in TIMES])/float(len(TIMES))
+        bpm = (1.0/(averagetime/60.0))
+
+        print("\nDetected BPM: %0.3f (Avg time between each: %0.3fs)"
+              % (bpm, averagetime), end='')
